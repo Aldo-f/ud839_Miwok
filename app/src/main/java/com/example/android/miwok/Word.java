d5c640521b4128d1d3f742d87018ea8c21274633
Aldo <aldofieuw@hotmail.com>
2017-05-25 21:39:35 +0200

    Visual

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,21 +8,27 @@ package com.example.android.miwok;
 
 public class Word {
 
-    /** default translation of the word **/
+    //    No image provides (in caps because its a constant)
+    private static final int NO_IMAGE_PROVIDED = -1;
+    /**
+     * default translation of the word
+     **/
     private String mDefaultTranslation;
-
-    /** MiwokTranslation of the word **/
+    /**
+     * MiwokTranslation of the word
+     **/
     private String mMiwokTranslation;
-
-    /** Image */
-    private int mImageResourceId;
+    /**
+     * Image
+     */
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
 
     /**
      * Create a new Word object.
      *
      * @param defaultTranslation is the word in a language that the user is already familiar with
      *                           (such as English)
-     * @param miwokTranslation is the word in the Miwok language
+     * @param miwokTranslation   is the word in the Miwok language
      */
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
@@ -34,9 +40,9 @@ public class Word {
      *
      * @param defaultTranslation is the word in a language that the user is already familiar with
      *                           (such as English)
-     * @param miwokTranslation is the word in the Miwok language
-     * @param imageResourceId is the drawable resource ID for the image associated with the word
-     * !!!!!!!!!!!!!!!!! same name, but with multiple usages (this one has an image int)
+     * @param miwokTranslation   is the word in the Miwok language
+     * @param imageResourceId    is the drawable resource ID for the image associated with the word
+     *                           !!!!!!!!!!!!!!!!! same name, but with multiple usages (this one has an image int)
      */
     public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
         mDefaultTranslation = defaultTranslation;
@@ -47,14 +53,14 @@ public class Word {
     /**
      * get the default translation of the word.
      */
-    public String getDefaultTranslation(){
+    public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
 
     /**
      * get the miwok translation of the word.
      */
-    public String getMiwokTranslation(){
+    public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
 
@@ -62,5 +68,10 @@ public class Word {
 
     public int getImageResourceId() {
         return mImageResourceId;
+    }
+
+//    is there an image for the word?
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -51,10 +51,18 @@ public class WordAdapter extends ArrayAdapter<Word>{
         defaultTextView.setText(currentWord.getDefaultTranslation());
 
         // Find the ImageView in the list_item.xml layout with the ID list_item_icon
-        ImageView iconView = (ImageView) listItemView.findViewById(R.id.resource_image_view);
-        // Get the image resource ID from the current AndroidFlavor object and
-        // set the image to iconView
-        iconView.setImageResource(currentWord.getImageResourceId());
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.resource_image_view);
+
+        if (currentWord.hasImage()) {
+            imageView.setImageResource(currentWord.getImageResourceId());
+
+            //make visible (when reusing)
+            imageView.setVisibility(View.VISIBLE);
+        }
+        else {
+            // hide imageview
+            imageView.setVisibility(View.GONE);
+        }
 
 
         // Return the whole list item layout (containing 2 TextViews)
