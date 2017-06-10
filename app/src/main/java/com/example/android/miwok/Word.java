cf993b18fce60c4ceb57b7bc5dc5ce40f8a9abd6
Aldo <aldofieuw@hotmail.com>
2017-06-10 19:37:50 +0200

    just before some big design changes

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -58,6 +58,16 @@ public class Word {
         mAudioResourceId = audioResourceId;
     }
 
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", mAudioResourceId=" + mAudioResourceId +
+                ", mImageResourceId=" + mImageResourceId +
+                '}';
+    }
+
     /**
      * Get the default translation of the word.
      */
